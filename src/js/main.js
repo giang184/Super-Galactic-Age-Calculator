25b15998f5d9a2b0237cacad42b64882dc1c4bb0
Andrew Giang <giang184@gmail.com>
2021-06-25 13:46:44 -0700

    test pass for yearsLeft() for Mercury

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,17 @@
 import $ from 'jquery'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './css/styles.css'
+import './../css/styles.css'
 import {Galatic} from './galatic.js'
 
-let myG = new Galatic();
+// let myG = new Galatic();
+$(document).ready(function () {
+  $("form#galatic").submit(function (event) {
+    event.preventDefault();
+
+    const dob = $("#born").val();
+    console.log(typeof(dob));
+    $("#result").text(dob);
+
+  });
+});
