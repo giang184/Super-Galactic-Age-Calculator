3d2a633350fa9999e8bc17338d27ecec715fba1e
Andrew Giang <giang184@gmail.com>
2021-06-25 14:53:37 -0700

    tests passed for yearsLeft() for the remaining planets

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './../css/styles.css'
 import {Galatic} from './galatic.js'
 
+const calculateAge = (date) => {
+  const today = new Date();
+  let yearDifference = today.getFullYear() - date[0];
+  let monthDifference = today.getMonth() + 1 - date[1];
+  let dayDifference = today.getDate() - date[2];
+  return (yearDifference + monthDifference/12 + dayDifference/365).toFixed(2);
+}
 // let myG = new Galatic();
 $(document).ready(function () {
   $("form#galatic").submit(function (event) {
     event.preventDefault();
 
     const dob = $("#born").val();
-    console.log(typeof(dob));
-    $("#result").text(dob);
+    let array = dob.split("-");
+    for(let i=0; i<array.length; i++) {
+      array[i]=parseInt(array[i]);
+    }
+
+    
+
+
+    $("#result").text(`you are ${calculateAge(array)} years old in Earth year`);
 
   });
 });
