007f1b850217cbd9e17b05351b0eabeae3998317
Andrew Giang <giang184@gmail.com>
2021-06-25 09:40:26 -0700

    add the first passed test for Galatic Constructor

--- __tests__/galatic.test.js
+++ __tests__/galatic.test.js
@@ -1,1 +1,11 @@
-import {Galatic} from './../src/galatic.js'
+import {Galatic} from './../src/js/galatic.js'
+
+describe('Galatic', () => {
+  
+  test('Galatic constructor should create an object with input age and default life expectancy', () => {
+    const andrew = new Galatic(37); 
+    expect(andrew.lifeExpectancy).toEqual(78.7);
+  });
+
+
+});
